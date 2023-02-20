79b0ff170f493e6d0ceeab8b30a5902be1bf61bd
Tobiloba Adebisi <oluwatobilobaadebisi6@gmail.com>
2023-02-20 16:20:06 +0000

    Not sure what's causing the bug

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ const notificationRoutes = require("./routes/notificationRoutes")
 const postRoutes = require("./routes/postRoutes")
 mongoose.set('strictQuery', true)
 
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 
 // Connect to mongo
 connectDB()
@@ -55,5 +55,5 @@ app.use((err, req, res, next) => {
 
 mongoose.connection.once('open', () => {
     console.log("Connected to MongoDb")
-    app.listen(process.env.PORT || PORT, console.log(`Server running on port ${PORT}`))
+    app.listen(PORT, console.log(`Server running on port ${PORT}`))
 })
