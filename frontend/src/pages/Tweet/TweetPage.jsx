25fda9db64b66d3d350ca243b4f1572262625bd4
Tobiloba Adebisi <oluwatobilobaadebisi6@gmail.com>
2023-02-26 09:24:25 +0000

    Made a tiny cosmetic change

--- frontend/src/pages/Tweet/TweetPage.jsx
+++ frontend/src/pages/Tweet/TweetPage.jsx
@@ -62,6 +62,7 @@ export default function TweetPage() {
               })
             );
             setComments([data.data, ...comments]);
+            setContent("");
           }}
         >
           Post
