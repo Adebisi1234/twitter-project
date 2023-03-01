aa6b48562906811b8f37d238175e1877f63e0aa0
Tobiloba Adebisi <oluwatobilobaadebisi6@gmail.com>
2023-03-01 10:46:32 +0000

    Working on tagging

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -9,7 +9,7 @@ const {
 } = require("../controllers/userController");
 const router = express.Router();
 
-router.get("/some:handle", getSomeUser);
+router.get("/some/:handle", getSomeUser);
 router.get("/get/:handle", getUser);
 router.post("/new-follower/:handle", newFollowers);
 router.post("/new-following/:handle", newFollowing);
