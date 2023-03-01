272fcee48b14fde35320253f2cc1060505985f40
Tobiloba Adebisi <oluwatobilobaadebisi6@gmail.com>
2023-03-01 10:45:59 +0000

    Working on tagging

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,12 +1,19 @@
-const express = require("express")
-const { getUser, newFollowers, newFollowing, editProfile, unFollow, getSomeUser } = require("../controllers/userController")
-const router = express.Router()
+const express = require("express");
+const {
+  getUser,
+  newFollowers,
+  newFollowing,
+  editProfile,
+  unFollow,
+  getSomeUser,
+} = require("../controllers/userController");
+const router = express.Router();
 
-router.get("/some", getSomeUser)
-router.get("/get/:handle", getUser)
-router.post("/new-follower/:handle", newFollowers)
-router.post("/new-following/:handle", newFollowing)
-router.post("/unfollow/", unFollow)
-router.post("/editProfile", editProfile)
+router.get("/some:handle", getSomeUser);
+router.get("/get/:handle", getUser);
+router.post("/new-follower/:handle", newFollowers);
+router.post("/new-following/:handle", newFollowing);
+router.post("/unfollow/", unFollow);
+router.post("/editProfile", editProfile);
 
-module.exports = router
+module.exports = router;
